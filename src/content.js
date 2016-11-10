f1b14d0e722fa13407278806d0e6a031506edcd2
John Freeman <jfreeman08@gmail.com>
2016-11-10 16:49:42 -0600

    Better React practices
    - ES6 classes instead of React.createClass
    - Functional stateless components instead of classes
    - Class properties instead of auto-binding

--- src/content.js
+++ src/content.js
@@ -1,59 +1,60 @@
 import React from 'react'
 import { Dropdown } from 'semantic-ui-react'
 
-var ButtonForm = React.createClass({
-    render() {
-        return (
-            <div>
-                <input type="submit" onClick={this.props.onUserClick} />
-                <h3>You have pressed the button {this.props.counter} times!</h3>
-            </div>
-        )
-    }
-})
+function ButtonForm(props) {
+  return (
+    <div>
+      <input type="submit" onClick={props.onUserClick} />
+      <h3>You have pressed the button {props.counter} times!</h3>
+    </div>
+  )
+}
+ButtonForm.propTypes = {
+  counter: React.PropTypes.number.isRequired,
+  onUserClick: React.PropTypes.func.isRequired,
+}
 
-var DictionarySelector = React.createClass({
-    getInitialState() {
-        return {value: ''}
-    },
-    render() {
-        let options = [
-            {value: 'fi', flag: 'fi', text: 'Finland'},
-            {value: 'de', flag: 'de', text: 'Germany'},
-            {value: 'mx', flag: 'mx', text: 'Mexico'},
-            {value: 'us', flag: 'us', text: 'United States'},
-            {value: 'vn', flag: 'vn', text: 'Vietnam'},
-        ]
-        return (
-            <div>
-                <Dropdown placeholder='Select Country' search selection options={options}
-                    onChange={ (_, {value}) => this.setState({value}) } />
-                <p>You selected: {this.state.value}</p>
-            </div>
-        )
-    }
-})
+class DictionarySelector extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {value: ''}
+  }
 
-var App = React.createClass({
-    getInitialState() {
-        return {
-            counter: 0
-        }
-    },
-    onUserClick() {
-        this.setState({
-            counter: this.state.counter + 1
-        })
-    },
-    render() {
-        return (
-            <div>
-                <h1>Hello, world!</h1>
-                <ButtonForm counter={this.state.counter} onUserClick={this.onUserClick} />
-                <DictionarySelector />
-            </div>
-        )
-    }
-})
+  render() {
+    let options = [
+      {value: 'fi', flag: 'fi', text: 'Finland'},
+      {value: 'de', flag: 'de', text: 'Germany'},
+      {value: 'mx', flag: 'mx', text: 'Mexico'},
+      {value: 'us', flag: 'us', text: 'United States'},
+      {value: 'vn', flag: 'vn', text: 'Vietnam'},
+    ]
+    return (
+      <div>
+        <Dropdown placeholder='Select Country' search selection options={options}
+          onChange={ (_, {value}) => this.setState({value}) } />
+        <p>You selected: {this.state.value}</p>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  onUserClick = () => this.setState((state) => ({counter: state.counter + 1}))
+
+  constructor(props) {
+    super(props)
+    this.state = {counter: 0}
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <ButtonForm counter={this.state.counter} onUserClick={this.onUserClick} />
+        <DictionarySelector />
+      </div>
+    )
+  }
+}
 
 module.exports = App
